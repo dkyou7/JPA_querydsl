0392eff93c4d6850e9b6841eafa11afb359cf434
youdongkwan <dkyou7@naver.com>
2020-11-26 23:48:05 +0900

    where Param으로 작성해보기

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -84,4 +84,31 @@ class MemberJpaRepositoryTest {
         List<MemberTeamDto> memberTeamDtos = memberJpaRepository.searchByBuilder(memberSearchCondition);
         assertThat(memberTeamDtos).extracting("username").contains("member4");
     }
+
+    @Test
+    public void searchTestByWhere(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",20,teamA);
+        Member member3 = new Member("member3",30,teamB);
+        Member member4 = new Member("member4",40,teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+        memberSearchCondition.setAgeGoe(35);
+        memberSearchCondition.setAgeLoe(45);
+        memberSearchCondition.setTeamName("teamB");
+
+        // 페이징 쿼리나 limit 이 들어가주는 것이 좋다. 장사 잘될 경우 조회 쿼리를 다 가져오기 때문이다.
+
+        List<MemberTeamDto> memberTeamDtos = memberJpaRepository.searchByWhereParam(memberSearchCondition);
+        assertThat(memberTeamDtos).extracting("username").contains("member4");
+    }
 }
