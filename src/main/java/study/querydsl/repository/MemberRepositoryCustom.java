53e8afabd66f9e85f3e87fc4a7e194e27c22fef2
youdongkwan <dkyou7@naver.com>
2020-11-28 00:51:51 +0900

    JPA Custom Repository 작성하기

---
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -0,0 +1,23 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+@Repository
+public interface MemberRepositoryCustom {
+    List<MemberTeamDto> searchByWhereParam(MemberSearchCondition condition);
+}
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -36,5 +39,30 @@ class MemberRepositoryTest {
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(mem);
     }
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
 
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",20,teamA);
+        Member member3 = new Member("member3",30,teamB);
+        Member member4 = new Member("member4",40,teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+        memberSearchCondition.setAgeGoe(35);
+        memberSearchCondition.setAgeLoe(45);
+        memberSearchCondition.setTeamName("teamB");
+
+        // 페이징 쿼리나 limit 이 들어가주는 것이 좋다. 장사 잘될 경우 조회 쿼리를 다 가져오기 때문이다.
+
+        List<MemberTeamDto> memberTeamDtos = memberRepository.searchByWhereParam(memberSearchCondition);
+        assertThat(memberTeamDtos).extracting("username").contains("member4");
+    }
 }
